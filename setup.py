14e0dbf7e14f7b910b36f6021349768b5d9e2f2a
Linas Valiukas <linas@media.mit.edu>
2018-11-29 16:34:43 +0000

    Require attrs >= 18.0.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=[
 
         # No dunder methods
-        'attrs>=17.4.0',
+        'attrs>=18.0.0',
 
         # Parsing arbitrary dates (sitemap date format is standardized but some implementations take liberties)
         'python-dateutil>=2.1,<3.0.0',
