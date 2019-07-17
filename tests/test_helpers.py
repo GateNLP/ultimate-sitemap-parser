e4ed5a5cef7a538d8ebb23b5059159a617bc39ae
Linas Valiukas <shirshegsm@gmail.com>
2019-07-17 17:51:55 +0300

    Rename date parsing helpers to more sensible names

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,7 +3,7 @@ import datetime
 import pytest
 
 from usp.exceptions import StripURLToHomepageException
-from usp.helpers import html_unescape_strip, parse_sitemap_publication_date, is_http_url, strip_url_to_homepage
+from usp.helpers import html_unescape_strip, parse_iso8601_date, is_http_url, strip_url_to_homepage
 
 
 def test_html_unescape_strip():
@@ -11,23 +11,23 @@ def test_html_unescape_strip():
     assert html_unescape_strip(None) is None
 
 
-def test_parse_sitemap_publication_date():
-    assert parse_sitemap_publication_date("1997-07-16") == datetime.datetime(year=1997, month=7, day=16)
-    assert parse_sitemap_publication_date("1997-07-16T19:20+01:00") == datetime.datetime(
+def test_parse_iso8601_date():
+    assert parse_iso8601_date("1997-07-16") == datetime.datetime(year=1997, month=7, day=16)
+    assert parse_iso8601_date("1997-07-16T19:20+01:00") == datetime.datetime(
         year=1997, month=7, day=16, hour=19, minute=20,
         tzinfo=datetime.timezone(datetime.timedelta(seconds=3600)),
     )
-    assert parse_sitemap_publication_date("1997-07-16T19:20:30+01:00") == datetime.datetime(
+    assert parse_iso8601_date("1997-07-16T19:20:30+01:00") == datetime.datetime(
         year=1997, month=7, day=16, hour=19, minute=20, second=30,
         tzinfo=datetime.timezone(datetime.timedelta(seconds=3600)),
     )
-    assert parse_sitemap_publication_date("1997-07-16T19:20:30.45+01:00") == datetime.datetime(
+    assert parse_iso8601_date("1997-07-16T19:20:30.45+01:00") == datetime.datetime(
         year=1997, month=7, day=16, hour=19, minute=20, second=30, microsecond=450000,
         tzinfo=datetime.timezone(datetime.timedelta(seconds=3600)),
     )
 
     # "Z" timezone instead of "+\d\d:\d\d"
-    assert parse_sitemap_publication_date("2018-01-12T21:57:27Z") == datetime.datetime(
+    assert parse_iso8601_date("2018-01-12T21:57:27Z") == datetime.datetime(
         year=2018, month=1, day=12, hour=21, minute=57, second=27, tzinfo=datetime.timezone.utc,
     )
 
