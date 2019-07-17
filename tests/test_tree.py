617676c6f09596d6ceac8bda5ad268e7000db641
Linas Valiukas <shirshegsm@gmail.com>
2019-07-17 18:01:11 +0300

    Use format_datetime() instead of formatdate()

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -2,7 +2,7 @@ import datetime
 import difflib
 import textwrap
 from decimal import Decimal
-from email.utils import formatdate
+from email.utils import format_datetime
 from http import HTTPStatus
 from unittest import TestCase
 
@@ -46,7 +46,7 @@ class TestSitemapTree(TestCase):
     TEST_DATE_STR_ISO8601 = TEST_DATE_DATETIME.isoformat()
     """Test string date formatted as ISO 8601 (for XML and Atom 0.3 / 1.0 sitemaps)."""
 
-    TEST_DATE_STR_RFC2822 = formatdate(float(TEST_DATE_DATETIME.strftime('%s')), localtime=True)
+    TEST_DATE_STR_RFC2822 = format_datetime(TEST_DATE_DATETIME)
     """Test string date formatted as RFC 2822 (for RSS 2.0 sitemaps)."""
 
     TEST_PUBLICATION_NAME = 'Test publication'
