095b8b0f5e3071e91e0460acf77d07dc279af05b
Freddy Heppell <freddy@freddyheppell.com>
2025-02-17 11:45:26 +0000

    reformat

--- tests/tree/test_opts.py
+++ tests/tree/test_opts.py
@@ -12,5 +12,11 @@ class TestTreeOpts(TreeTestBase):
         return mocker.patch("usp.tree.SitemapFetcher")
 
     def test_extra_known_paths(self, mock_fetcher):
-        sitemap_tree_for_homepage("https://example.org", extra_known_paths={"custom_sitemap.xml"})
-        mock_fetcher.assert_any_call(url="https://example.org/custom_sitemap.xml", web_client=mock.ANY, recursion_level=0)
+        sitemap_tree_for_homepage(
+            "https://example.org", extra_known_paths={"custom_sitemap.xml"}
+        )
+        mock_fetcher.assert_any_call(
+            url="https://example.org/custom_sitemap.xml",
+            web_client=mock.ANY,
+            recursion_level=0,
+        )
