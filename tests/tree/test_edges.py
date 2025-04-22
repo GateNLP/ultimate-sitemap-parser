f4dc79725e5c08ccf9068b3bcc858ff4411ebafa
Freddy Heppell <freddy@freddyheppell.com>
2025-04-22 11:25:46 +0100

    parse sitemaps without proper ns

--- tests/tree/test_edges.py
+++ tests/tree/test_edges.py
@@ -1,8 +1,14 @@
 import textwrap
+from decimal import Decimal
 
 from tests.tree.base import TreeTestBase
+from usp.objects.page import SitemapPage, SitemapPageChangeFrequency
 from usp.objects.sitemap import (
+    IndexRobotsTxtSitemap,
+    IndexWebsiteSitemap,
+    IndexXMLSitemap,
     InvalidSitemap,
+    PagesXMLSitemap,
 )
 from usp.tree import sitemap_tree_for_homepage
 
@@ -227,3 +233,91 @@ class TestTreeEdgeCases(TreeTestBase):
         all_pages = list(tree.all_pages())
         assert len(all_pages) == 49
         assert all_pages[-1].url.endswith("page_48.html")
+
+    def test_sitemap_no_ns(self, requests_mock, caplog):
+        requests_mock.add_matcher(TreeTestBase.fallback_to_404_not_found_matcher)
+
+        requests_mock.get(
+            self.TEST_BASE_URL + "/robots.txt",
+            headers={"Content-Type": "text/plain"},
+            text=textwrap.dedent(
+                f"""
+                        User-agent: *
+                        Disallow: /whatever
+
+                        Sitemap: {self.TEST_BASE_URL}/sitemap_index.xml
+                    """
+            ).strip(),
+        )
+
+        requests_mock.get(
+            self.TEST_BASE_URL + "/sitemap_index.xml",
+            headers={"Content-Type": "application/xml"},
+            text=textwrap.dedent(
+                f"""
+                        <?xml version="1.0" encoding="UTF-8"?>
+                        <sitemapindex>
+                            <sitemap>
+                                <loc>{self.TEST_BASE_URL}/sitemap_pages.xml</loc>
+                                <lastmod>{self.TEST_DATE_STR_ISO8601}</lastmod>
+                            </sitemap>
+                        </sitemapindex>
+                    """
+            ).strip(),
+        )
+
+        # random_tag is to check assuming sitemap namespace doesn't cause issues
+        requests_mock.get(
+            self.TEST_BASE_URL + "/sitemap_pages.xml",
+            headers={"Content-Type": "application/xml"},
+            text=textwrap.dedent(
+                f"""
+                        <?xml version="1.0" encoding="UTF-8"?>
+                        <urlset>
+                            <url>
+                                <loc>{self.TEST_BASE_URL}/about.html</loc>
+                                <lastmod>{self.TEST_DATE_STR_ISO8601}</lastmod>
+                                <changefreq>monthly</changefreq>
+                                <priority>0.8</priority>
+                                <random_tag>random_value</random_tag>
+                            </url>
+                        </urlset>
+                    """
+            ).strip(),
+        )
+
+        expected_sitemap_tree = IndexWebsiteSitemap(
+            url=f"{self.TEST_BASE_URL}/",
+            sub_sitemaps=[
+                IndexRobotsTxtSitemap(
+                    url=f"{self.TEST_BASE_URL}/robots.txt",
+                    sub_sitemaps=[
+                        IndexXMLSitemap(
+                            url=f"{self.TEST_BASE_URL}/sitemap_index.xml",
+                            sub_sitemaps=[
+                                PagesXMLSitemap(
+                                    url=f"{self.TEST_BASE_URL}/sitemap_pages.xml",
+                                    pages=[
+                                        SitemapPage(
+                                            url=f"{self.TEST_BASE_URL}/about.html",
+                                            last_modified=self.TEST_DATE_DATETIME,
+                                            change_frequency=SitemapPageChangeFrequency.MONTHLY,
+                                            priority=Decimal("0.8"),
+                                        )
+                                    ],
+                                )
+                            ],
+                        )
+                    ],
+                )
+            ],
+        )
+
+        actual_sitemap_tree = sitemap_tree_for_homepage(homepage_url=self.TEST_BASE_URL)
+
+        assert expected_sitemap_tree == actual_sitemap_tree
+        assert (
+            'sitemapindex detected without expected xmlns (value is "")' in caplog.text
+        )
+        assert 'urlset detected without expected xmlns (value is "")' in caplog.text
+        assert "Assuming random_tag should be in sitemap namespace" in caplog.text
